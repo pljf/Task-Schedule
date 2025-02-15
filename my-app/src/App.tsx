a4f85440fb5ae53b6ecaa076023869bb195bbecd
pljf <patrick.jf.luo@gmail.com>
2025-02-15 02:21:20 -0500

    yzc 3

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,20 +3,19 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <div>Hello World !!!!
+        <br />
         <br />
         <br />
         <hr />
         <a href="https://www.google.com" target="_blank">link to google</a>
       </div>
-=======
       <section id="Header"></section>
       <section id="task-input"> </section>
       <section id="task-schedule"></section>
       <section id="animation"></section>      
->>>>>>> 78a90e0e6676f0da187e17908507abf75b586cf5
     </>
+    
   )
 }
 
