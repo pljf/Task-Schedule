1313434d701832aa616d78d40b26752fd9db0105
unknown <yuzhicheng369@gmail.com>
2025-02-15 00:19:25 -0500

    init

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,25 @@
+import './Header.css';
+import './task-input.css';
+import './task-input.css';
+import './animation.css';
+
 function App() {
   /*Javascript Function*/
 
   return (
     <>
-      <section id="Header"></section>
-      <section id="task-input"> </section>
-      <section id="task-schedule">Hello world hello</section>
+      <section id="Header">
+        <div className='header-format'>
+        <div>X</div>
+        <div><strong>Task Scheduler</strong></div>
+        <div>X</div>
+        </div>
+      </section>
+      
+      <section id="task-input"></section>
+      <section id="task-schedule">
+
+      </section>
       <section id="animation"></section>      
     </>
     
